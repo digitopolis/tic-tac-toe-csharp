ebd5ef7e7ac21d7118cd88b0441d4adc25f56dfb
Matt Readout <matt.readout@gmail.com>
2020-02-19 14:54:10 -0800

    test that string gets output to console

--- TicTacToe.Tests/CLITests.cs
+++ TicTacToe.Tests/CLITests.cs
@@ -1,15 +1,23 @@
 using System;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace TicTacToe.Tests
 {
     public class CLITests
     {
+
         [Fact]
-        public void TestLogMessage()
+        public void TestLogToConsole()
         {
             var cli = new CLI();
-            cli.LogToConsole();
+            string message = "test message";
+            var currentConsoleOut = Console.Out;
+            using (var consoleOutput = new ConsoleOutput())
+            {
+              cli.LogToConsole(message);
+              Assert.Equal(message, consoleOutput.GetOutput());
+            }
         }
     }
 }
