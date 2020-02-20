95826828e91e349afbedfb1e0f0fd36b8d5d2f1f
Matt Readout <matt.readout@gmail.com>
2020-02-20 15:59:54 -0800

    add marker to player

--- TicTacToe.Tests/PlayerTests.cs
+++ TicTacToe.Tests/PlayerTests.cs
@@ -6,16 +6,25 @@ namespace TicTacToe.Tests
 {
     public class PlayerTests
     {
+        Player player = new Player("player");
+
         [Fact]
-        public void CanCreateNewPlayer(){
-            Player player = new Player("player");
+        public void CanCreateNewPlayer() => Assert.IsType<Player>(player);
+
+        [Fact]
+        public void PlayerHasName()
+        {
+            var name = "Matt";
+            player.Name = name;
+            Assert.Equal(name, player.Name);
         }
 
         [Fact]
-        public void PlayerHasName(){
-            var name = "Matt";
-            Player player = new Player(name);
-            Assert.Equal(name, player.Name);
+        public void PlayerHasMarker()
+        {
+            char marker = 'X';
+            player.Marker = marker;
+            Assert.Equal(marker, player.Marker);
         }
     }
 }
--- TicTacToe/Player.cs
+++ TicTacToe/Player.cs
@@ -10,5 +10,6 @@ namespace TicTacToe
         }
 
         public string Name { get; set; }
+        public char Marker { get; set; }
     }
 }
