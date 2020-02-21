b9fdf1aaa53812f13cb7eba2845d414d3d127f22
Matt Readout <matt.readout@gmail.com>
2020-02-21 09:53:31 -0800

    add game.MakeMove()

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -12,5 +12,11 @@ namespace TicTacToe
         public char[] Board { get; set; }
         public Player Player1 { get; set; }
         public Player Player2 { get; set; }
+
+        public void MakeMove(int space, char marker)
+        {
+            int index = space - 1;
+            this.Board[index] = marker;
+        }
     }
 }
