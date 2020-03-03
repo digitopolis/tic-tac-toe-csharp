88a7b233bc0b06fff05dc8b8151bc3ff3b6bb89e
Matt Readout <matt.readout@gmail.com>
2020-03-03 10:56:28 -0800

    FindBestMove determines if move results in win

--- TicTacToe/ComputerPlayer.cs
+++ TicTacToe/ComputerPlayer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace TicTacToe
 {
@@ -6,7 +7,26 @@ namespace TicTacToe
     {
         public int FindBestMove(Game game)
         {
-            
+            for (int s = 1; s <= game.Board.Length; s++)
+            {
+                if (game.SpaceIsAvailable(s))
+                {
+                    int index = s - 1;
+                    game.Board[index] = 'X';
+                    if (game.WinningCombinations().Contains(true))
+                    {
+                        game.Board[index] = char.Parse(s.ToString());
+                        return s;
+                    }
+                    game.Board[index] = 'O';
+                    if (game.WinningCombinations().Contains(true))
+                    {
+                        game.Board[index] = char.Parse(s.ToString());
+                        return s;
+                    }
+                }
+            }
+            return -1;
         }
     }
 }
